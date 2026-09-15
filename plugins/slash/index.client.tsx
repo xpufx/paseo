de538938bcfb52849d7393027c8699118952a9ec
xpufx <github@xpufx.com>
2026-09-15 20:18:11 +0000

    feat(slash): console as sidebar surface with navbar entry

--- plugins/slash/index.client.tsx
+++ plugins/slash/index.client.tsx
@@ -1,12 +1,19 @@
 import type { PluginClientContext } from "@getpaseo/plugin/client";
 import { useRpc } from "@getpaseo/plugin/client";
 import { Icon, Modal, useToast, ScrollView, FlatList, TextInput as HostTextInput, copyText } from "@getpaseo/plugin/client/react-native";
-import { initClientHelpers } from "./client/vendor/paseo-plugin-helper/index";
+import { initClientHelpers, registerSidebarSurface } from "./client/vendor/paseo-plugin-helper/index";
 import { registerSlashCommands } from "./client/commands";
+import { SlashConsole } from "./client/console";
 
 initClientHelpers({ Icon, Modal, useRpc, useToast, copyText, ScrollView, FlatList, TextInput: HostTextInput });
 
 export default function contribute(client: PluginClientContext) {
+  registerSidebarSurface(client, {
+    id: "slash-console",
+    title: "Slash console",
+    icon: "Terminal",
+    Component: SlashConsole,
+  });
   const removeCommands = registerSlashCommands(client);
   return () => {
     removeCommands();
